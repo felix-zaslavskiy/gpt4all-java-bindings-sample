f479d523faf5e8ba12b1a24a2e42e32709e1c8e5
felix <felix@zaslavskiy.net>
2023-06-08 04:58:23 -0400

    latest

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gpt4all-java-bindings-sample
-A sample project that uses GPT4ALL Java bindings
+A sample project that uses GPT4ALL Java bindings.
+
+This project depends on the latest released version the bindings package.
--- src/main/java/com/hexadevlabs/gpt4allsample/Main.java
+++ src/main/java/com/hexadevlabs/gpt4allsample/Main.java
@@ -10,11 +10,16 @@ public class Main {
 
     public static void main(String[] args){
 
+        // DIRECTION: Update the baseModelPath to the location of model files on your system.
+
         String modelFilePath = "ggml-gpt4all-j-v1.3-groovy.bin";
-        //String baseModelPath = "C:\\Users\\felix\\AppData\\Local\\nomic.ai\\GPT4All\\";
+        String baseModelPath = "C:\\Users\\felix\\AppData\\Local\\nomic.ai\\GPT4All\\";
+
         // Alt path for WSL Linux
-        String baseModelPath = "/mnt/c/Users/felix/AppData/Local/nomic.ai/GPT4All/";
-        String librarySearchPath = "C:\\Users\\felix\\gpt4all\\lib\\";
+        //String baseModelPath = "/mnt/c/Users/felix/AppData/Local/nomic.ai/GPT4All/";
+
+        // Optionally in case override to location of shared libraries is necessary
+        //LLModel.LIBRARY_SEARCH_PATH = "C:\\Users\\felix\\gpt4all\\lib\\";
 
         if(args.length>0){
             if(args[0].equals("mpt")){
@@ -23,19 +28,22 @@ public class Main {
                 modelFilePath = "ggml-vicuna-7b-1.1-q4_2.bin";
             }
         }
-        // Optionally in case extra search path are necessary.
-        LLModel.LIBRARY_SEARCH_PATH = librarySearchPath;
+
+        // Debut output format. In case you need it.
+        // LLModel.OUTPUT_DEBUG=true;
 
         Path modelPath = Path.of(baseModelPath + modelFilePath);
 
-        LLModel.GenerationConfig config = LLModel.config()
-                .withNPredict(4096).build();
-
         try ( LLModel model = new LLModel(modelPath) ){
+
+            LLModel.GenerationConfig config = LLModel.config()
+                    .withNPredict(4096).build();
+
+            // String result = gptjModel.generate(prompt, config, true);
             model.chatCompletion(
-                    List.of(Map.of("role", "user", "content", "Add 2+2"),
-                            Map.of("role", "assistant", "content", "4"),
-                            Map.of("role", "user", "content", "Multiply 4 * 5")), config, true, true);
+                    List.of(Map.of("role", "system", "content", "You are a helpful assistant"),
+                            Map.of("role", "user", "content", "Add 2+2")), config, true, true);
+
 
         } catch (Exception e) {
             throw new RuntimeException(e);
